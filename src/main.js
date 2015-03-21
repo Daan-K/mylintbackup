a21aa1a15fc34defb9852c72055da2a419493a0d
Adolfo Ochagavía <aochagavia92@gmail.com>
2015-03-21 11:01:19 +0100

    Progress

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
-var prs = [];
+var exec = require('child_process').spawn;
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Nothing here...');
 });
 
+// This is used for debugging and should be removed later on
+var prs = [];
 app.get('/open-prs', function (req, res) {
     res.send(prs);
 });
 
+// Triggered by a new pull request
 app.post('/new-pr', function (req, res) {
-    console.log(req.body);
+    console.log(req.body);asdf
     prs.push(req.body);
     res.send('OK');
+    
+    // Run the linter
+    // spawn('./linter', ['arg1, arg2']);
 });
 
 var server = app.listen(3000, function () {
@@ -23,6 +29,6 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('App listening at http://%s:%s', host, port);
 
 });
